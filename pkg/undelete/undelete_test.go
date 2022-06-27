c7935f2aff6a1885ac06edfe386b54c5bea8984f
James Lucktaylor <jlucktay+github@gmail.com>
2022-06-27 11:32:46 +0100

    test(pkg/undelete): cover non-existent working directory error

--- pkg/undelete/undelete_test.go
+++ pkg/undelete/undelete_test.go
@@ -250,3 +250,41 @@ func TestUndeleteWithoutPrepareFails(t *testing.T) {
 	is.True(errors.As(err, &target)) // error should be of type *os.LinkError
 	is.Equal(target.Op, "rename")    // error operation should be 'rename'
 }
+
+func TestNewErrorsOnMissingWorkingDirectory(t *testing.T) {
+	// Arrange
+	is := is.New(t)
+
+	wd, err := os.Getwd()
+	is.NoErr(err) // can't get working directory
+
+	tmp, err := os.MkdirTemp(filepath.Join(wd, "testdata"), "temporary")
+	is.NoErr(err) // can't create temp directory
+
+	t.Cleanup(func() {
+		is.NoErr(os.RemoveAll(tmp)) // cleanup can't remove temp directory
+	})
+
+	basePath := filepath.Join(tmp, "Deleted Games and Apps")
+
+	is.NoErr(os.MkdirAll(basePath, 0o750)) // can't create temp base path
+	is.NoErr(os.Chdir(basePath))           // can't change working directory to temp base path
+
+	t.Cleanup(func() {
+		is.NoErr(os.Chdir(wd)) // cleanup can't revert working directory
+	})
+
+	is.NoErr(os.RemoveAll(tmp)) // can't remove temp directory
+
+	// Act
+	_, err = undelete.New()
+
+	// Assert
+	is.True(err != nil) // undelete.New should error when working directory does not exist
+
+	var target *fs.PathError
+
+	is.True(errors.As(err, &target)) // error should be of type *fs.PathError
+	is.Equal(target.Op, "open")      // error operation should be 'open'
+	is.Equal(target.Path, basePath)  // error path should match temp base path
+}
